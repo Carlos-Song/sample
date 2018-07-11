7e0bdd1fbdadeda633264e512b1f7934f07853b7
Silverwing <silverwingsw@icloud.com>
2018-07-11 19:26:52 +0000

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,15 +1,20 @@
-<html lang="en">
-<head>
-    <title>@yield('title', 'Sample') - Laravel 入门</title>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
+  </head>
+  <body>
     @include('layouts._header')
 
     <div class="container">
+      <div class="col-md-offset-1 col-md-10">
         @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
+      </div>
     </div>
-</body>
+
+    <script src="/js/app.js"></script>
+  </body>
 </html>
